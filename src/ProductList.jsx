63e5d6eaf9153aeb9bcf5a959178071ae4d53fb3
navarro-juan <navarrojgr@gmail.com>
2024-09-30 23:39:54 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -270,7 +270,7 @@ const handleContinueShopping = (e) => {
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a href="/shoppingcart/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
+                    <h3 style={{color:'white'}}>Rooted Revival</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
                     </div>
                     </a>
